0f2e5bb98b30da810381b75239940b5dd439e0e8
hriday <hriday.bavle@gmail.com>
2017-09-15 10:18:37 +0200

    updated drone2

--- drone2/mission.py
+++ drone2/mission.py
@@ -1,17 +1,18 @@
 import executive_engine_api as api
 
 def runMission():
-    api.activateBehavior('SELF_LOCALIZE_BY_ODOMETRY')
-    api.executeBehavior('TAKE_OFF')
-    api.inhibitBehavior('SELF_LOCALIZE_BY_ODOMETRY')
-
+    # api.activateBehavior('SELF_LOCALIZE_BY_ODOMETRY')
     api.activateBehavior('SELF_LOCALIZE_BY_VISUAL_MARKER')
+    api.executeBehavior('TAKE_OFF')
+    # api.inhibitBehavior('SELF_LOCALIZE_BY_ODOMETRY')
+
+    # api.activateBehavior('SELF_LOCALIZE_BY_VISUAL_MARKER')
     api.executeBehavior('GO_TO_POINT', coordinates=[0, -1.3, 1.3])
-    api.executeBehavior('GO_TO_POINT', coordinates=[3.75, -1.3, 1.3])
+    api.executeBehavior('GO_TO_POINT', coordinates=[3.4, -1.3, 1.3])
     # api.executeBehavior('GO_TO_POINT', coordinates=[3, -1.3, 1.3])
     # api.executeBehavior('GO_TO_POINT', coordinates=[5.5, -0.6, 1.3])
     # api.executeBehavior('GO_TO_POINT', coordinates=[7.6, 0.3, 1.3])
-    api.executeBehavior('ROTATE', angle=90)
+    # api.executeBehavior('ROTATE', angle=90)
     api.executeBehavior('ROTATE', angle=180)
     # api.executeBehavior('GO_TO_POINT', coordinates=[7.5, -1.5, 1.3])
     # api.executeBehavior('ROTATE', angle=135)
@@ -21,9 +22,10 @@ def runMission():
     # api.executeBehavior('GO_TO_POINT', coordinates=[3.75, 0.6, 1.3])
     # api.executeBehavior('GO_TO_POINT', coordinates=[0, 0.6, 1.3])
     # api.executeBehavior('GO_TO_POINT', coordinates=[10.5, -3.5, 1.3])
-    api.executeBehavior('GO_TO_POINT', coordinates=[0, 0, 1.3])
-    api.executeBehavior('ROTATE', angle=90)
-    api.executeBehavior('ROTATE', angle=0)
+    api.executeBehavior('GO_TO_POINT', coordinates=[0, -1.3, 1.3])
+    api.executeBehavior('GO_TO_POINT', coordinates=[0, 0, 2])
+    # api.executeBehavior('ROTATE', angle=90)
+    # api.executeBehavior('ROTATE', angle=0)
     # api.executeBehavior('GO_TO_POINT', coordinates=[0, 0, 1.5])
     api.executeBehavior('LAND_ON_STATIC_PLATFORM')
     api.inhibitBehavior('SELF_LOCALIZE_BY_VISUAL_MARKER')
