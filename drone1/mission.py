4af8e6c27312825981d93dd7ceeec05c5e944bb6
Guillermo De Fermin <gdefermin@gmail.com>
2017-09-16 15:42:08 +0200

    final commit before toulouse

--- drone1/mission.py
+++ drone1/mission.py
@@ -1,34 +1,56 @@
 import executive_engine_api as api
 import time
 
+QR_LOCATION = [13.1, 1]
+
 def runMission():
     api.activateBehavior('SELF_LOCALIZE_BY_VISUAL_MARKER')
+    # api.activateBehavior('SELF_LOCALIZE_BY_ODOMETRY')
+
+    # time.sleep(120)
 
     print("Taking off..")
     result = api.executeBehavior('TAKE_OFF')
     print("TAKE_OFF ended with status %s" % result)
 
-    goThroughWindow()
+    # Pose: [0, 0, 0.47, 0]
+    # goThroughWindow()
+
+    # Pose: [3.5, 0.3, 0, 0]
     goThroughPoles()
+
+    # Pose: [7.7, -0.5, 0]
     goThroughVegetation()
+
+    # Pose: [12.2, 0, 0, 180]
     lookForQR()
 
     api.inhibitBehavior('SELF_LOCALIZE_BY_VISUAL_MARKER')
+    # api.inhibitBehavior('SELF_LOCALIZE_BY_ODOMETRY')
     api.executeBehavior('LAND')
 
 def goThroughWindow():
-    point_a = [0, 0.6, 1.2]
+    point_a = [-0.5, 0, 1.2]
+    point_a_2 = [-0.5, 4.75, 1.2]
     print("Going to %s ..." % point_a)
     result = api.executeBehavior('GO_TO_POINT', coordinates=point_a)
     print("GO_TO_POINT ended with status %s" % result)
+    print("Going to %s ..." % point_a_2)
+    result = api.executeBehavior('GO_TO_POINT', coordinates=point_a_2)
+    print("GO_TO_POINT ended with status %s" % result)
 
-    point_b = [3, 0.6, 1.2]
+    point_b = [3.5, 4.75, 1.2]
     print("Going to %s ..." % point_b)
     result = api.executeBehavior('GO_TO_POINT', coordinates=point_b)
     print("GO_TO_POINT ended with status %s" % result)
 
 def goThroughPoles():
-    point_c = [8, 0, 1.2]
+    point_b = [3.8, 5.3, 1.2]
+    print("Going to %s ..." % point_b)
+    result = api.executeBehavior('GO_TO_POINT', coordinates=point_b)
+    print("GO_TO_POINT ended with status %s" % result)
+
+    point_c = [7.8, 4, 1.2]
     print("Going to %s ..." % point_c)
     result = api.executeBehavior('GO_TO_POINT', coordinates=point_c)
     print("GO_TO_POINT ended with status %s" % result)
@@ -50,27 +72,32 @@ def goThroughVegetation():
     # result = api.executeBehavior('ROTATE', angle=180)
     # print("ROTATE ended with status %s" % result)
 
-    point_d = [10.5, 0.5, 1.25]
-    print("Going to %s ..." % point_d)
-    result = api.executeBehavior('GO_TO_POINT', coordinates=point_d)
-    print("GO_TO_POINT ended with status %s" % result)
+    # point_d = [10.5, 0.5, 1.25]
+    # print("Going to %s ..." % point_d)
+    # result = api.executeBehavior('GO_TO_POINT', coordinates=point_d)
+    # print("GO_TO_POINT ended with status %s" % result)
 
-    point_e = [13.5, 0.5, 1.25]
+    point_e = [12.5, 4.8, 1.25]
     print("Going to %s ..." % point_e)
     result = api.executeBehavior('GO_TO_POINT', coordinates=point_e)
     print("GO_TO_POINT ended with status %s" % result)
 
-    point_f = [12.5, -2.5, 1.25]
+    # print("Rotating...")
+    # result = api.executeBehavior('ROTATE', angle=180)
+    # print("ROTATE ended with status %s" % result)
+
+
+def lookForQR():
+    point_f = [QR_LOCATION[0], QR_LOCATION[1], 1.25]
     print("Going to %s ..." % point_f)
     result = api.executeBehavior('GO_TO_POINT', coordinates=point_f)
     print("GO_TO_POINT ended with status %s" % result)
 
-def lookForQR():
     print("Looking for QR...")
     result = api.executeBehavior('RECOGNIZE_QR')
     if result != 'GOAL_ACHIEVED':
         print("QR not found")
-        api.executeBehavior('GO_TO_POINT', coordinates=[13, -2.5, 1.5])
+        api.executeBehavior('GO_TO_POINT', coordinates=[QR_LOCATION[0], QR_LOCATION[1], 2])
     else:
         print("QR found!")
     result = api.executeBehavior('RECOGNIZE_QR')
--- drone4/set_pose.sh
+++ drone4/set_pose.sh
@@ -9,3 +9,7 @@ sed -i "s#<y_0> .* </y_0>#<y_0> $2 </y_0>#g" params_localization_obs.xml
 sed -i "s#<z> .* </z>#<z> $3 </z>#g" ekf_state_estimator_config.xml
 sed -i "s#<z_0> .* </z_0>#<z_0> $3 </z_0>#g" params_localization_obs.xml
 sed -i "s#<z_0> .* </z_0>#<z_0> $3 </z_0>#g" robot_localization.xml
+
+sed -i "s#<yaw> .* </yaw>#<yaw> $4 </yaw>#g" ekf_state_estimator_config.xml
+sed -i "s#<yaw_0> .* </yaw_0>#<yaw_0> $4 </yaw_0>#g" params_localization_obs.xml
+sed -i "s#<yaw_0> .* </yaw_0>#<yaw_0> $4 </yaw_0>#g" robot_localization.xml
