4c472199dada0d9683acf15002f8a2c775db3bf7
ManuelRucci <ruccimanuel7@gmail.com>
2017-09-19 10:42:22 +0200

    Updated mission 3

--- drone3/mission3.py
+++ drone3/mission3.py
@@ -1,4 +1,5 @@
 import executive_engine_api as api
+import time
 
 def runMission():
     api.executeBehavior('TAKE_OFF')
@@ -6,3 +7,7 @@ def runMission():
     api.executeBehavior('MOVE_TO_VISUAL_MARKER', marker_id=7)
     api.inhibitBehavior('PAY_ATTENTION_TO_VISUAL_MARKERS')
     api.executeBehavior('LAND_ON_STATIC_PLATFORM')
+    time.sleep(2)
+    api.executeBehavior('TAKE_OFF')
+
+      
